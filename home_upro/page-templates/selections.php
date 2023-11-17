f11c3d84d0bd32c4c245753d12c7dea13c430f24
Erique-dn <vcslvperminov@gmail.com>
2023-11-17 13:07:09 +0300

    Update selections.php

--- home_upro/page-templates/selections.php
+++ home_upro/page-templates/selections.php
@@ -33,68 +33,71 @@ if($wp_query->have_posts()):
                 <?php endif ?>
 
             </div>
+            <div class="btn-wrap btn-add">
+                <a href="<?php the_permalink(1719) . '?client_name=' . get_field('buyer_name', $_GET['selection_id']) ?>"><img src="<?= get_stylesheet_directory_uri() ?>/img/icon-100.svg" alt=""><?php _e('додати підбір', 'Home') ?></a>
+            </div>
             <div class="content">
 
                 <?php while ($wp_query->have_posts()): $wp_query->the_post(); ?>
 
                     <?php if (get_field('buyer_name') == get_field('buyer_name', $_GET['selection_id'])): ?>
 
-                        <?php $selection_id = get_the_ID() ?>
+                    <?php $selection_id = get_the_ID() ?>
 
-                        <div class="item-photo" object_id="<?= $_GET['object_id'] ?>" selection_id="<?= $selection_id ?>">
-                            <div class="wrap">
-                                <h2>
-                                    <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-                                </h2>
-                                <p class="date"><?= get_the_date('d.m.Y') ?></p>
-                                <div class="btn-wrap">
-                                    <a href="#" class="delete-item-photo delete_selection"><img src="<?= get_stylesheet_directory_uri() ?>/img/icon-11.svg" alt=""></a>
+                    <div class="item-photo" object_id="<?= $_GET['object_id'] ?>" selection_id="<?= $selection_id ?>">
+                        <div class="wrap">
+                            <h2>
+                                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                            </h2>
+                            <p class="date"><?= get_the_date('d.m.Y') ?></p>
+                            <div class="btn-wrap">
+                                <a href="#" class="delete-item-photo delete_selection"><img src="<?= get_stylesheet_directory_uri() ?>/img/icon-11.svg" alt=""></a>
 
-                                    <?php $selection_objects = get_field('objects', $selection_id, false) ?>
+                                <?php $selection_objects = get_field('objects', $selection_id, false) ?>
 
-                                    <?php if ($_GET['object_id']): ?>
-                                        <div class="add_object_to_selection<?php if(in_array($_GET['object_id'], $selection_objects)) echo ' is_added' ?>">
-                                            <a href="#">
-                                                <img src="<?= get_stylesheet_directory_uri() ?>/img/add-to-selection.svg" alt="">
-                                            </a>
-                                        </div>
-                                    <?php endif ?>
+                                <?php if ($_GET['object_id']): ?>
+                                    <div class="add_object_to_selection<?php if(in_array($_GET['object_id'], $selection_objects)) echo ' is_added' ?>">
+                                        <a href="#">
+                                            <img src="<?= get_stylesheet_directory_uri() ?>/img/add-to-selection.svg" alt="">
+                                        </a>
+                                    </div>
+                                <?php endif ?>
 
-                                    <a href="<?php the_permalink() ?>" class="share"><img src="<?= get_stylesheet_directory_uri() ?>/img/icon-12.svg" alt=""></a>
-                                </div>
+                                <a href="<?php the_permalink() ?>" class="share"><img src="<?= get_stylesheet_directory_uri() ?>/img/icon-12.svg" alt=""></a>
+                            </div>
+                        </div>
+
+                        <?php
+                        $selection_objects = get_field('objects');
+                        if($selection_objects): ?>
+
+                            <div class="wrap-photo">
+
+                                <?php foreach($selection_objects as $post):
+
+                                    global $post;
+                                    setup_postdata($post); ?>
+                                    <div class="wrap-selection">
+                                        <a href="#"><?= get_the_post_thumbnail($post->ID, 'full') ?: '<img src="' . get_stylesheet_directory_uri() . '/img/icon-10.svg" alt="">' ?></a>
+
+                                    </div>
+                                <?php endforeach; ?>
+
+                                <?php wp_reset_postdata(); ?>
+
                             </div>
 
-                            <?php
-                            $selection_objects = get_field('objects');
-                            if($selection_objects): ?>
+                        <?php endif; ?>
 
-                                <div class="wrap-photo">
-
-                                    <?php foreach($selection_objects as $post):
-
-                                        global $post;
-                                        setup_postdata($post); ?>
-                                        <div class="wrap-selection">
-                                            <a href="#"><?= get_the_post_thumbnail($post->ID, 'full') ?: '<img src="' . get_stylesheet_directory_uri() . '/img/icon-10.svg" alt="">' ?></a>
-
-                                        </div>
-                                    <?php endforeach; ?>
-
-                                    <?php wp_reset_postdata(); ?>
-
-                                </div>
-
-                            <?php endif; ?>
-
-                        </div>
-                    <?php endif ?>
+                    </div>
+                <?php endif ?>
 
 
-                <?php endwhile; ?>
+            <?php endwhile; ?>
 
-            </div>
         </div>
-    </section>
+    </div>
+</section>
 
 <?php
 endif;
