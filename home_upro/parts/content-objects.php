9e65e1ba06073fcc613ab34ad2e9ce0f0aa5fba4
Erique-dn <vcslvperminov@gmail.com>
2023-12-21 15:19:38 +0300

    Update content-objects.php

--- home_upro/parts/content-objects.php
+++ home_upro/parts/content-objects.php
@@ -148,12 +148,21 @@
 
 <?php endif ?>
 
-<?php if (get_field('superficiality') && get_field('over') && $object_type->term_id != 13): ?>
+<?php if (get_field('superficiality') && get_field('over') && $object_type->term_id != 8 && $object_type->term_id != 9 && $object_type->term_id != 11 && $object_type->term_id != 13): ?>
 <li>
 	<div class="img-wrap">
 		<img src="<?= get_stylesheet_directory_uri() ?>/img/icon-7-3.svg" alt="">
 	</div>
 	<p><?= get_field('over') . ' ' .  __('з', 'Home') . ' ' . get_field('superficiality') ?></p>
+</li>
+<?php endif ?>
+
+<?php if (get_field('number_of_floors') && ($object_type->term_id == 8 || $object_type->term_id == 9 || $object_type->term_id == 11)): ?>
+<li>
+	<div class="img-wrap">
+		<img src="<?= get_stylesheet_directory_uri() ?>/img/icon-7-3.svg" alt="">
+	</div>
+	<p><?= get_field('number_of_floors') ?></p>
 </li>
 <?php endif ?>
 
