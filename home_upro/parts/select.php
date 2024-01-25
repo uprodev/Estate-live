06f23ce3afac0bd4e312c5de3310aa1923262e43
Erique-dn <vcslvperminov@gmail.com>
2024-01-25 13:55:43 +0300

    Update select.php

--- home_upro/parts/select.php
+++ home_upro/parts/select.php
@@ -1,19 +1,18 @@
 <?php $terms = get_field('cities_header', 'option') ?>
 
-<?php if ($terms): ?>
+<?php if ($terms): 
 
-<?php 
-if(is_singular('objects')){
-    $object_terms = wp_get_object_terms(get_the_ID(), 'city');
-    if($object_terms){
-        $parent_term_id = '';
-        foreach ($object_terms as $object_term) {
-            if($object_term->parent == 0) $parent_term_id[] = $object_term->term_id;
+    if(is_singular('objects')){
+        $object_terms = wp_get_object_terms(get_the_ID(), 'city');
+        if($object_terms){
+            $parent_term_id = '';
+            foreach ($object_terms as $object_term) {
+                if($object_term->parent == 0) $parent_term_id = $object_term->name;
+            }
         }
     }
-}
-else $parent_term_id = get_field('title_add', 'term_' . $terms[0]->term_id);
-?>
+    else $parent_term_id = get_field('title_add', 'term_' . $terms[0]->term_id);
+    ?>
 
     <div class="nice-select">
         <span class="current"><?= $_GET['region_id'] ? get_field('title_add', 'term_' . $_GET['region_id']) : $parent_term_id ?></span>
