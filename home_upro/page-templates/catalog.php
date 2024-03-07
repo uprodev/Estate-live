9497b1667ce30bc9113291ce8d5ab336c910288e
Erique-dn <vcslvperminov@gmail.com>
2024-03-07 12:11:17 +0300

    update

--- home_upro/page-templates/catalog.php
+++ home_upro/page-templates/catalog.php
@@ -40,11 +40,11 @@ $wp_query = new WP_Query(array(
 
 		<?php if($wp_query->have_posts()): ?>
 
-			<?php /*$current_user_id = get_current_user_id()*/ ?>
+			<?php $current_user_id = get_current_user_id() ?>
 
 			<?php while ($wp_query->have_posts()): $wp_query->the_post(); ?>
 
-				<?php get_template_part('parts/content', 'objects', ['object_id' => get_the_ID(), 'current_user_id' => $current_user_id]) ?>
+				
 
 			<?php endwhile; ?>
 
