004f83bf3f6e2fc1411d3e3c01eaafa83c3ba993
Erique-dn <vcslvperminov@gmail.com>
2024-03-07 11:51:21 +0300

    update

--- home_upro/parts/content-objects.php
+++ home_upro/parts/content-objects.php
@@ -1,4 +1,4 @@
-123<?php $object_type = wp_get_object_terms(get_the_ID(), 'object_type')[0] ?>
+<?php $object_type = wp_get_object_terms(get_the_ID(), 'object_type')[0] ?>
 
 <div class="item-home">
 	<figure>
