ca1dd893f4099dccfe7e5b94c7928adb78f339c7
Vyacheslav <105778715+Erique-dn@users.noreply.github.com>
2024-04-29 17:47:39 +0300

    Update add.js

--- home_upro/js/add.js
+++ home_upro/js/add.js
@@ -34,13 +34,14 @@ jQuery(document).ready(function($) {
 					let builders = JSON.parse(data);
 					$('ul#get_builders').empty();
 					$('ul#get_builders').append('<li class="option"><label for="builder-0"></label><input type="radio" id="builder-0" name="meta_builder" value="">Всі</li>');
-					$('ul#get_complexes').empty();
-					$('ul#get_complexes').append('<li class="option"><label for="complex-0"></label><input type="radio" id="complex-0" name="meta_complex" value="">Всі</li>');
 					$.each(builders, function(index, value){
 						$('ul#get_builders').append(`<li class="option"><label for="builder-${index + 1}"></label><input type="radio" name="meta_builder" id="builder-${index + 1}" value="${value.ID}">${value.post_title}</li>`);
 					});
 				} else {
-					console.log('Error!');
+					$('ul#get_builders').empty();
+					$('ul#get_builders').append('<li class="option"><label for="builder-0"></label><input type="radio" id="builder-0" name="meta_builder" value="">Всі</li>');
+					$('ul#get_complexes').empty();
+					$('ul#get_complexes').append('<li class="option"><label for="complex-0"></label><input type="radio" id="complex-0" name="meta_complex" value="">Всі</li>');
 				}
 			},
 		});
@@ -69,7 +70,8 @@ jQuery(document).ready(function($) {
 						$('ul#get_complexes').append(`<li class="option"><label for="complex-${index + 1}"></label><input type="radio" name="meta_complex" id="complex-${index + 1}" value="${value.ID}">${value.post_title}</li>`);
 					});
 				} else {
-					console.log('Error!');
+					$('ul#get_complexes').empty();
+					$('ul#get_complexes').append('<li class="option"><label for="complex-0"></label><input type="radio" id="complex-0" name="meta_complex" value="">Всі</li>');
 				}
 			},
 		});
